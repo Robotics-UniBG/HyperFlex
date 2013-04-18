c588054c21db4adcccd6aec8ad2054299d736215
lucaghera <lucaghera8@gmail.com>
2013-04-18 13:00:52 +0200

    Added new file created with for the Containment Association

---
+++ it.unibg.robotics.featuremodels.model.diagram/src/it/unibg/robotics/featuremodels/model/diagram/edit/commands/FeatureContainersCreateCommand.java
@@ -0,0 +1,124 @@
+/*
+ * Feature Model Plugin
+ * 
+ * Copyright (c) 2011
+ * All rights reserved.
+ * 
+ * Luca Gherardi
+ * University of Bergamo
+ * Dept. of Information Technology and Mathematics
+ * 
+ * ***********************************************************************************************
+ * 
+ * Author: <A HREF="mailto:luca.gherardi@unibg.it">Luca Gherardi</A>
+ * 
+ * Supervised by: <A HREF="mailto:brugali@unibg.it">Davide Brugali</A>
+ * 
+ * ***********************************************************************************************
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * 
+ */
+package it.unibg.robotics.featuremodels.model.diagram.edit.commands;
+
+import it.unibg.robotics.featuremodels.ContainmentAssociation;
+import it.unibg.robotics.featuremodels.Feature;
+import it.unibg.robotics.featuremodels.model.diagram.edit.policies.FeatureModelBaseItemSemanticEditPolicy;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.emf.type.core.commands.EditElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.CreateRelationshipRequest;
+
+/**
+ * @generated
+ */
+public class FeatureContainersCreateCommand extends EditElementCommand {
+
+	/**
+	 * @generated
+	 */
+	private final EObject source;
+
+	/**
+	 * @generated
+	 */
+	private final EObject target;
+
+	/**
+	 * @generated
+	 */
+	public FeatureContainersCreateCommand(CreateRelationshipRequest request,
+			EObject source, EObject target) {
+		super(request.getLabel(), null, request);
+		this.source = source;
+		this.target = target;
+	}
+
+	/**
+	 * @generated
+	 */
+	public boolean canExecute() {
+		if (source == null && target == null) {
+			return false;
+		}
+		if (source != null && false == source instanceof Feature) {
+			return false;
+		}
+		if (target != null && false == target instanceof ContainmentAssociation) {
+			return false;
+		}
+		if (getSource() == null) {
+			return true; // link creation is in progress; source is not defined yet
+		}
+		// target may be null here but it's possible to check constraint
+		return FeatureModelBaseItemSemanticEditPolicy.getLinkConstraints()
+				.canCreateFeatureContainers_4015(getSource(), getTarget());
+	}
+
+	/**
+	 * @generated NOT
+	 */
+	protected CommandResult doExecuteWithResult(IProgressMonitor monitor,
+			IAdaptable info) throws ExecutionException {
+		if (!canExecute()) {
+			throw new ExecutionException(
+					"Invalid arguments in create link command"); //$NON-NLS-1$
+		}
+
+		if (getSource() != null && getTarget() != null) {
+			getSource().getContainers().add(getTarget());
+			getTarget().setParent(getSource());
+		}
+		return CommandResult.newOKCommandResult();
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void setElementToEdit(EObject element) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Feature getSource() {
+		return (Feature) source;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected ContainmentAssociation getTarget() {
+		return (ContainmentAssociation) target;
+	}
+}
---
+++ it.unibg.robotics.featuremodels.model.diagram/src/it/unibg/robotics/featuremodels/model/diagram/edit/policies/FeatureContainersItemSemanticEditPolicy.java
@@ -0,0 +1,73 @@
+/*
+ * Feature Model Plugin
+ * 
+ * Copyright (c) 2011
+ * All rights reserved.
+ * 
+ * Luca Gherardi
+ * University of Bergamo
+ * Dept. of Information Technology and Mathematics
+ * 
+ * ***********************************************************************************************
+ * 
+ * Author: <A HREF="mailto:luca.gherardi@unibg.it">Luca Gherardi</A>
+ * 
+ * Supervised by: <A HREF="mailto:brugali@unibg.it">Davide Brugali</A>
+ * 
+ * ***********************************************************************************************
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * 
+ */
+package it.unibg.robotics.featuremodels.model.diagram.edit.policies;
+
+import it.unibg.robotics.featuremodels.model.diagram.providers.FeatureModelElementTypes;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.emf.type.core.commands.DestroyReferenceCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyReferenceRequest;
+
+/**
+ * @generated
+ */
+public class FeatureContainersItemSemanticEditPolicy extends
+		FeatureModelBaseItemSemanticEditPolicy {
+
+	/**
+	 * @generated
+	 */
+	public FeatureContainersItemSemanticEditPolicy() {
+		super(FeatureModelElementTypes.FeatureContainers_4015);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Command getDestroyReferenceCommand(DestroyReferenceRequest req) {
+		return getGEFWrapper(new DestroyReferenceCommand(req) {
+			protected CommandResult doExecuteWithResult(
+					IProgressMonitor progressMonitor, IAdaptable info)
+					throws ExecutionException {
+				EObject referencedObject = getReferencedObject();
+				Resource resource = referencedObject.eResource();
+				CommandResult result = super.doExecuteWithResult(
+						progressMonitor, info);
+				if (resource != null) {
+					resource.getContents().add(referencedObject);
+				}
+				return result;
+			}
+		});
+	}
+
+}
